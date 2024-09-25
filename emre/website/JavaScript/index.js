4f4d56e9bd390c7c4d0ac10b949e43be90c1cb00
Emre Cakmakkaya <emre@MacBook-Pro-von-Emre.local>
2024-09-25 16:47:00 +0200

    25.09.2024

---
+++ emre/website/JavaScript/index.js
@@ -0,0 +1,7 @@
+var anfangskapital = parseFloat(document.getElementById("anfangskapital").value)
+var sparrate = parseFloat(document.getElementById("sparrate").value)
+var laufzeit = parseFloat(document.getElementById("laufzeit").value)
+var zinssatz = parseFloat(document.getElementById("zinssatz").value)
+function berechnen() {
+    document.getElementById("ergebnis").value=3
+}
