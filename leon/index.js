87d7585c289ec4733e34a84672d3c440e8c97bee
leon-buening <leon.buening@gmail.com>
2024-10-09 16:44:29 +0200

    09-10-2024

--- leon/index.js
+++ leon/index.js
@@ -1,8 +1,32 @@
 function human() {
-let sum = parseFloat(document.getElementById("renteein").value) - parseFloat(document.getElementById("alter").value);
+   ein = document.getElementById("renteein")
+   alter = document.getElementById("alter")
+   steigerung = 1 + (parseFloat(document.getElementById("steigerung").value) * 0.01)
+   gehalt = parseFloat(document.getElementById("gehalt").value)
+x = 0
+y = 0
+   gehalt1 = parseFloat(document.getElementById("gehalt").value)
+
+
+let sum = parseFloat(ein.value) - parseFloat(alter.value);
 document.getElementById("arbeitsjahre").innerText = sum;
-let f = Math.pow(parseFloat(document.getElementById("steigerung").value) * parseFloat(document.getElementById("gehalt").value),sum);
-document.getElementById("vinfla").innerText = f;
+
+//let f = gehalt *Math.pow( steigerung ,(sum * 12));
+
+/*for (let i = 1; i < (sum * 12); i++) {
+gehalt = gehalt + (gehalt1 * steigerung)
+}*/
 
 
+while (y<(sum * 12))   {
+
+     if(x=13){
+      gehalt = gehalt + (gehalt1 * steigerung)
+      x = 0
+     }
+      gehalt = gehalt + gehalt1
+      x += 1
+      y += 1
+   }
+document.getElementById("vinfla").innerText = gehalt;
 }
