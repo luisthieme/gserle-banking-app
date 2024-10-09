8c34597e656fe17d020692ae950cc2ff8022eb4a
Zoe2908 <zoeweiss2908@gmail.com>
2024-10-09 16:51:16 +0200

    09-10-1024

---
+++ Zoe/index.js
@@ -0,0 +1,54 @@
+function berechnenEins() {
+    if(document.getElementById('mySelect').value == "optionEins") {
+        var phase = parseFloat(document.getElementById("myForm").elements["phase"].value);
+        var abgeltung = parseFloat(document.getElementById("myForm").elements["abgeltung"].value);
+        var ziel = parseFloat(document.getElementById("myForm").elements["ziel"].value);
+        var zinssatz = parseFloat(document.getElementById("myForm").elements["zinssatz"].value);
+        var effektiverZinssatz =  zinssatz * (1 - (abgeltung / 100))
+        var monatlicherZinssatz = (effektiverZinssatz / 12) / 100
+        var monatlichPhase = phase * 12
+        var sparrate = ziel * monatlicherZinssatz / (Math.pow((1 + monatlicherZinssatz), monatlichPhase) - 1)
+        sparrate = sparrate.toFixed(2);
+        document.getElementById("pEins").innerHTML = sparrate;
+    }
+    else {
+        berechnenZwei ();
+    }
+}
+
+function berechnenZwei() {
+    if(document.getElementById('mySelect').value == "optionZwei") {
+        var phase = parseFloat(document.getElementById("myForm").elements["phase"].value);
+        var abgeltung = parseFloat(document.getElementById("myForm").elements["abgeltung"].value);
+        var ziel = parseFloat(document.getElementById("myForm").elements["ziel"].value);
+        var zinssatz = parseFloat(document.getElementById("myForm").elements["zinssatz"].value);
+        var abVoKi = (8 * abgeltung)  / 100
+        var abUnKi = abgeltung + abVoKi
+        var effektiverZinssatz =  zinssatz * (1 - abUnKi / 100)
+        var monatlicherZinssatz = (effektiverZinssatz / 12) / 100
+        var monatlichPhase = phase * 12
+        var sparrate = ziel * monatlicherZinssatz / (Math.pow((1 + monatlicherZinssatz), monatlichPhase) - 1)
+        sparrate = sparrate.toFixed(2);
+        document.getElementById("pEins").innerHTML = sparrate;
+    }
+    else {
+        berechnenDrei ();
+    }
+}
+
+function berechnenDrei() {
+    if(document.getElementById('mySelect').value == "optionDrei") {
+        var phase = parseFloat(document.getElementById("myForm").elements["phase"].value);
+        var abgeltung = parseFloat(document.getElementById("myForm").elements["abgeltung"].value);
+        var ziel = parseFloat(document.getElementById("myForm").elements["ziel"].value);
+        var zinssatz = parseFloat(document.getElementById("myForm").elements["zinssatz"].value);
+        var abVoKi = (abgeltung * 9) / 100
+        var abUnKi = abgeltung + abVoKi
+        var effektiverZinssatz =  zinssatz * (1 - abUnKi / 100)
+        var monatlicherZinssatz = (effektiverZinssatz / 12) / 100
+        var monatlichPhase = phase * 12
+        var sparrate = ziel * monatlicherZinssatz / (Math.pow((1 + monatlicherZinssatz), monatlichPhase) - 1)
+        sparrate = sparrate.toFixed(2);
+        document.getElementById("pEins").innerHTML = sparrate;
+    }
+}
