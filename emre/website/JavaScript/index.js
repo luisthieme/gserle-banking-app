2fa5938beaace94948225b99c3d60c787a9abb4a
Emre Cakmakkaya <emre@MacBook-Pro-von-Emre.local>
2024-10-30 16:34:03 +0100

    30.10.2024

--- emre/website/JavaScript/index.js
+++ emre/website/JavaScript/index.js
@@ -1,61 +1,62 @@
-document.getElementById("anfangskapitalInput").value = 23
-var anfangskapital = parseFloat(document.getElementById("anfangskapitalInput").value)
-var laufzeit = parseFloat(document.getElementById("laufzeit").value)
-var zinssatz = parseFloat(document.getElementById("zinssatz").value)
-var endkapital = parseFloat(document.getElementById("endkapital").value)
+document.getElementById("anfangskapitalInput").value = 23;
+var anfangskapital = parseFloat(
+  document.getElementById("anfangskapitalInput").value
+);
+var laufzeit = parseFloat(document.getElementById("laufzeit").value);
+var zinssatz = parseFloat(document.getElementById("zinssatz").value);
+var endkapital = parseFloat(document.getElementById("endkapital").value);
 
+var dropdownIntervall = document.getElementById("auswahlSparintervall");
+var dropdownRechner = document.getElementById("auswahlRechner");
 
+var Rechner = dropdownRechner.value;
+console.log("Ausgewählter Rechner:", Rechner);
 
-var dropdown = document.getElementById('auswahlSparintervall');
-dropdown.addEventListener('change', function() {
-    var ausgewählterWert = dropdown.value;
-    console.log('Ausgewähltes Sparintervall:', ausgewählterWert);
+var Sparintervall = dropdownIntervall.value;
+console.log("Ausgewähltes Sparintervall:", Sparintervall);
+
+dropdownIntervall.addEventListener("change", function () {
+  var Sparintervall = dropdownIntervall.value;
+  console.log("Ausgewähltes Sparintervall:", Sparintervall);
 });
 
-    var ausgewaelterRechnerDropdown = document.getElementById("auswahlRechner")
-    ausgewaelterRechnerDropdown.addEventListener('change', function() {
-    var ausgewaelterRechner = ausgewaelterRechnerDropdown.value
-    if(ausgewaelterRechner == "anfangskapital"){
-        document.getElementById("anfangskapitalClass").style.display="none"
-        document.getElementById("sparrateClass").style.display="block"
-        document.getElementById("laufzeitClass").style.display="block"
-        document.getElementById("endkapitalClass").style.display="block"
-        document.getElementById("zinssatzClass").style.display="block"
-    }
-    else if(ausgewaelterRechner=="zinssatz"){
-        document.getElementById("anfangskapitalClass").style.display="block"
-        document.getElementById("sparrateClass").style.display="block"
-        document.getElementById("laufzeitClass").style.display="block"
-        document.getElementById("endkapitalClass").style.display="block"
-        document.getElementById("zinssatzClass").style.display="none"
-    }
-    else if(ausgewaelterRechner=="laufzeit"){
-        document.getElementById("anfangskapitalClass").style.display="block"
-        document.getElementById("sparrateClass").style.display="block"
-        document.getElementById("laufzeitClass").style.display="none"
-        document.getElementById("endkapitalClass").style.display="block"
-        document.getElementById("zinssatzClass").style.display="block"
-    }
-    else if(ausgewaelterRechner=="endkapital"){
-        document.getElementById("anfangskapitalClass").style.display="block"
-        document.getElementById("sparrateClass").style.display="block"
-        document.getElementById("laufzeitClass").style.display="block"
-        document.getElementById("endkapitalClass").style.display="none"
-        document.getElementById("zinssatzClass").style.display="block"
-    }
-    else if(ausgewaelterRechner=="sparrate"){
-        document.getElementById("anfangskapitalClass").style.display="block"
-        document.getElementById("sparrateClass").style.display="none"
-        document.getElementById("laufzeitClass").style.display="block"
-        document.getElementById("endkapitalClass").style.display="block"
-        document.getElementById("zinssatzClass").style.display="block"
-    }
-    console.log("Ausgewählter Rechner:", ausgewaelterRechner);
+var ausgewaelterRechnerDropdown = document.getElementById("auswahlRechner");
+ausgewaelterRechnerDropdown.addEventListener("change", function () {
+  var ausgewaelterRechner = ausgewaelterRechnerDropdown.value;
+  if (ausgewaelterRechner == "anfangskapital") {
+    document.getElementById("anfangskapitalClass").style.display = "none";
+    document.getElementById("sparrateClass").style.display = "block";
+    document.getElementById("laufzeitClass").style.display = "block";
+    document.getElementById("endkapitalClass").style.display = "block";
+    document.getElementById("zinssatzClass").style.display = "block";
+  } else if (ausgewaelterRechner == "zinssatz") {
+    document.getElementById("anfangskapitalClass").style.display = "block";
+    document.getElementById("sparrateClass").style.display = "block";
+    document.getElementById("laufzeitClass").style.display = "block";
+    document.getElementById("endkapitalClass").style.display = "block";
+    document.getElementById("zinssatzClass").style.display = "none";
+  } else if (ausgewaelterRechner == "laufzeit") {
+    document.getElementById("anfangskapitalClass").style.display = "block";
+    document.getElementById("sparrateClass").style.display = "block";
+    document.getElementById("laufzeitClass").style.display = "none";
+    document.getElementById("endkapitalClass").style.display = "block";
+    document.getElementById("zinssatzClass").style.display = "block";
+  } else if (ausgewaelterRechner == "endkapital") {
+    document.getElementById("anfangskapitalClass").style.display = "block";
+    document.getElementById("sparrateClass").style.display = "block";
+    document.getElementById("laufzeitClass").style.display = "block";
+    document.getElementById("endkapitalClass").style.display = "none";
+    document.getElementById("zinssatzClass").style.display = "block";
+  } else if (ausgewaelterRechner == "sparrate") {
+    document.getElementById("anfangskapitalClass").style.display = "block";
+    document.getElementById("sparrateClass").style.display = "none";
+    document.getElementById("laufzeitClass").style.display = "block";
+    document.getElementById("endkapitalClass").style.display = "block";
+    document.getElementById("zinssatzClass").style.display = "block";
+  }
+  console.log("Ausgewählter Rechner:", ausgewaelterRechner);
 });
 function berechnen() {
-    var ausgewaelterRechner = document.getElementById("auswahlRechner").value
-
+  var ausgewaelterRechner = document.getElementById("auswahlRechner").value;
 }
-function test() {
-
-}
+function test() {}
