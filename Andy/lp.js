4962b77af7762cc8dd151e03be2b9ec0da1e42b3
Andy <schlecht.andy@icloud.com>
2024-10-30 16:39:28 +0100

    30.10.2024

--- Andy/lp.js
+++ Andy/lp.js
@@ -1,4 +1,5 @@
-show=true
+document.getElementById('JetztAlter').addEventListener('input', JahrebisFF);
+document.getElementById('WunschAlter').addEventListener('input', JahrebisFF);
 function JahrebisFF() {
     JetzigesAlter= parseInt(document.getElementById("JetztAlter").value);
     Wunschrenteneintritsalter= parseInt(document.getElementById("WunschAlter").value);
@@ -14,4 +15,18 @@ function KapitalverzehrAn() {
     document.getElementById("Kapitalverzehr").style.display = "block"
     document.getElementById("Lebenserwartung").style.display = "block"
 }
+function berechnen(){
+    Startkapital=parseInt(document.getElementById("Kapital").value);
+    MonatlicherWunschbetrag=parseInt(document.getElementById("MonatlicherWunschbetrag").value);
+    Laufzeit=parseInt(document.getElementById("JahrebisFF").innerHTML);
+    JahrRendite=parseFloat(document.getElementById("xRendite").value)/100;
+    MonatsRendite=JahrRendite/12;
+    Endkapital=1/JahrRendite*MonatlicherWunschbetrag*12;
+    Test=Endkapital
+    //while (passt=false) {
+    //    for ( n=0; <; ++) {        
+    //    }
+    //}
+}
+
 
