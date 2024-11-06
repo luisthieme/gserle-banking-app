a77a9afb058aca079ec68ac33c3a010309b53e0f
Illia Barsukov <illiabarsukov147@gmail.com>
2024-11-06 16:15:59 +0100

    06.11.2024

--- Projekt Illia Infltionsrechner/Rechner.js
+++ Projekt Illia Infltionsrechner/Rechner.js
@@ -1,3 +1,24 @@
-Endbetrag (Startbetrag, Inflationsrate, Zeitraum) {
-  return(Startbetrag*(1+Inflationsrate)^Zeitraum)
+let a = ''; // Ursprungsbetrag
+let b = ''; // Inflationsrate
+let c = ''; // Zeit
+let Berechnungsart = ''; 
+let finish = false;
+
+const out = document.querySelector('.calc-screen p');
+function clearAll () {
+    a = ''; // Ursprungsbetrag
+    b = ''; // Inflationsrate
+    c = ''; // Zeit
+    Berechnungsart = ''; 
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ls').onclick = clearAll;
+
+document.querySelector('.Knopf').onclick = (Event) => {
+   if(!Event.target.classList.Contains('Kn')) return;
+   if(Event.target.classList.Contains('ls')) return;
+
+   get ()
 }
