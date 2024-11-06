1a06244e30cad539989942751dd0c08b1616e677
Andy <schlecht.andy@icloud.com>
2024-11-06 16:08:02 +0100

    06.11.2024

--- Andy/lp.js
+++ Andy/lp.js
@@ -18,15 +18,16 @@ function KapitalverzehrAn() {
 function berechnen(){
     Startkapital=parseInt(document.getElementById("Kapital").value);
     MonatlicherWunschbetrag=parseInt(document.getElementById("MonatlicherWunschbetrag").value);
-    Laufzeit=parseInt(document.getElementById("JahrebisFF").innerHTML);
+    Laufzeit=parseInt(document.getElementById("JahrebisFF").innerHTML)*12;
     JahrRendite=parseFloat(document.getElementById("xRendite").value)/100;
-    MonatsRendite=JahrRendite/12;
-    Endkapital=1/JahrRendite*MonatlicherWunschbetrag*12;
-    Test=Endkapital
-    //while (passt=false) {
-    //    for ( n=0; <; ++) {        
-    //    }
-    //}
+    Abgeltungssteuer=parseFloat(document.getElementById("Abgeltungssteuer").value)/100;
+    NettoRendite=JahrRendite*(1-Abgeltungssteuer)
+    Kapitalbedarf=(1/NettoRendite)*MonatlicherWunschbetrag*12;
+    MonatlicheSparrate = parseInt((Kapitalbedarf-Startkapital*Math.pow(1+NettoRendite/12,Laufzeit)) / ((Math.pow(1 + NettoRendite / 12, Laufzeit) - 1) / (NettoRendite / 12)));
+    Endkapital = parseInt(Startkapital * Math.pow(1 + NettoRendite / 12, Laufzeit)+ MonatlicheSparrate * ((Math.pow(1 + NettoRendite / 12, Laufzeit) - 1) / (NettoRendite / 12)));
+    document.getElementById("Berechnung").style.display = "block"
+    document.getElementById("Ergebnisse").innerHTML ="Zusätzlich benötigtes Kapital: "+parseInt(Kapitalbedarf-Startkapital).toLocaleString()+"€"+"<br>"+"Monatliche Sparrate: "+parseInt(MonatlicheSparrate).toLocaleString()+"€";
+    document.getElementById("Erklärung").innerHTML ="Bei einer monatlichen Sparrate von "+MonatlicheSparrate.toLocaleString()+"€ hast du nach "+Laufzeit/12+" Jahren ein Endkapital von "+Endkapital.toLocaleString()+"€ zusammen. Bei "+JahrRendite*100+"% angelegt kannst du mit einer monatlichen Rente von "+MonatlicherWunschbetrag.toLocaleString() +"€ rechnen."
 }
 
 
