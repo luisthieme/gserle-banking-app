86a455119251a780ad943e16664350ce16eafbf1
Tudor <tudor.gales@gmail.com>
2024-12-11 16:31:59 +0100

    11.12.2024

--- HTML 5Minds/5Minds-AG/index.js
+++
@@ -1,3 +0,0 @@
-function berechnen() {
-    console.log(678)
-}
---
+++ nextjs/test/src/app/page.js
@@ -0,0 +1,21 @@
+"use client";
+import { useState } from "react";
+export default function Home() {
+  
+  function coun(){
+    setCounter(counter+1);
+  }
+  
+  const [counter, setCounter] = useState(19)
+  return (
+    <div>
+      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+      <h6>densetu no onara masuta</h6>
+      <p>________________________________________________________________________________________________</p>
+      <p>________________________________________________________________________________________________</p>
+      <button onClick={coun}>click.</button>
+      <p>________________________________________________________________________________________________</p>
+      <div>{counter}</div>
+    </div>
+  );
+}
