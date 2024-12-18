ee37a01f420bc8becfb3cfde80a0b1d8d1519236
Tudor <tudor.gales@gmail.com>
2024-12-18 15:17:05 +0100

    12. 18. 2024, Anfang

--- HTML 5Minds/nextjs/test/src/app/page.js
+++ HTML 5Minds/nextjs/test/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState } from "react; 
 export default function Home() {
   
   function coun(){
---
+++ HTML 5Minds/nextjs/test/src/app/stift/page.js
@@ -0,0 +1,8 @@
+"use Client"
+export default function Page() {
+    return (
+        <div>
+            <h4>Test</h4>
+        </div>
+    )
+}
